5d06f1cbbd788ec453e4530e29a333c4a90f6e2a
str22ui <sbhakti22@gmail.com>
2024-05-17 08:46:35 +0700

    update faq

--- resources/views/landingPage/index.blade.php
+++ resources/views/landingPage/index.blade.php
@@ -79,6 +79,21 @@
 
     <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+    <script>
+        function toggleText(answerId, questionId) {
+            var x = document.getElementById(answerId);
+            var y = document.getElementById(questionId);
+            if (x.style.display === "none") {
+                x.style.display = "flex";
+                x.style.backgroundColor = "lightblue";
+                y.classList.add("text-color2"); // Add color to the question
+            } else {
+                x.style.display = "none";
+                y.classList.remove("text-color2"); // Remove color from the question
+            }
+        }
+    </script>
+    </script>
 </body>
 
 </html>
